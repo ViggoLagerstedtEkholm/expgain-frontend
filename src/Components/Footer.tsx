601c1259f22edc96125e235806bb0227993482d0
Viggo Lagerstedt Ekholm <viggo.lagerstedtekholm@afry.com>
2022-01-27 01:52:49 +0100

    Addeed styling.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -16,7 +16,7 @@ function Footer() {
                         </li>
                         <li>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua
+                            eiusmod tempor incididunt ut labore et dolore magna aliquadd
                         </li>
                         <li>+1-543-123-4567</li>
                         <li>example@expgain.com</li>
